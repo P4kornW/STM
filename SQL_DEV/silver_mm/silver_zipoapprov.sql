cbba22f1ccfaa48aa7c49b0108bffc8bcc064837
Pakorn Surakarnkul <pakorn.surakarn@gmail.com>
2025-12-17 17:45:24 +0700

    update silver layer poapprove

--- SQL_DEV/silver_mm/silver_zipoapprov.sql
+++ SQL_DEV/silver_mm/silver_zipoapprov.sql
@@ -1,27 +1,32 @@
 WITH Ranked_Raw_Batch AS (
-    -- LEVEL 1: DEDUPLICATION (The "Filter")
-    -- We select * from Bronze to get all columns, calculate Rank, and filter bad flags.
-    SELECT 
-        *, 
+    SELECT
+        *,
         ROW_NUMBER() OVER (
             PARTITION BY purchasingdocument
-            ORDER BY ingestiontime DESC
+            ORDER BY approvedate DESC, approvetime DESC, ingestiontime DESC
         ) AS rn
     FROM zipoapprov
     WHERE
         purchasingdocument IS NOT NULL
-
+        -- AND isdelete = false
+        AND isapprove = 'Yes'
 )
 
-SELECT DISTINCT
-    CAST(R.purchasingdocument AS STRING) AS purchasingdocument,
-    TO_TIMESTAMP(CONCAT(R.approvedate, R.approvetime),'yyyyMMddHHmmss') AS approve_dt,
-    CAST(R.isapprove AS STRING) AS isapprove,
-    R.ingestiontime,
-    R.isupsert,
-    R.isdelete,
-    R.isinsert,
-    R.changetype,
-    current_timestamp() as last_modified_dt
+SELECT
+    CAST(purchasingdocument AS STRING) AS purchasingdocument,
+    TO_TIMESTAMP(
+        CONCAT(approvedate, approvetime),
+        'yyyyMMddHHmmss'
+    ) AS approve_dt,
+    approvercode,
+    approveusername,
+    approverdescription,
+    CAST(isapprove AS STRING) AS isapprove,
+    ingestiontime,
+    isupsert,
+    isdelete,
+    isinsert,
+    changetype,
+    current_timestamp() AS last_modified_dt
 FROM Ranked_Raw_Batch R
 WHERE rn = 1
