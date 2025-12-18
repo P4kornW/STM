b255362b07115a16158f79bf43fd5533253505f3
P4kornW <pakorn@blendata.com>
2025-12-18 16:43:37 +0700

    add praprrove silver table

---
+++ SQL_DEV/silver_mm/silver_ziprapprov.sql
@@ -0,0 +1,30 @@
+WITH Ranked_Raw_Batch AS (
+    SELECT
+        *,
+        ROW_NUMBER() OVER (
+            PARTITION BY tabkey
+            ORDER BY ingestiontime DESC
+        ) AS rn
+    FROM ziprapprov
+    WHERE
+        tabkey IS NOT NULL
+        -- AND isdelete = false
+)
+
+SELECT
+    CAST(R.tabkey AS STRING) AS tabkey,
+    CAST(R.purchasereqnitemuniqueid AS STRING) AS purchasereqnitemuniqueid,
+    to_date(R.updatedate, 'yyyyMMdd') AS updatedate,
+    CAST(R.updatetime AS STRING) AS updatetime,
+    TO_TIMESTAMP(
+        CONCAT(R.updatedate, R.updatetime),
+        'yyyyMMddHHmmss'
+    ) AS update_dt,
+    R.ingestiontime,
+    R.isupsert,
+    R.isdelete,
+    R.isinsert,
+    R.changetype,
+    current_timestamp() AS last_modified_dt
+FROM Ranked_Raw_Batch R
+WHERE rn = 1
