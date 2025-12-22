a9fb977928b17e74ef76944a84adc015621f1d61
Pakorn Surakarnkul <pakorn.surakarn@gmail.com>
2025-12-22 17:06:53 +0700

    mart layer pohistory as main join with item and header

---
+++ SQL_DEV/silver_mm/silver_mm_plus/silver_plus_historypo.sql
@@ -0,0 +1,108 @@
+ WITH silver_zimmpurdochist_cte AS (
+        SELECT
+        purchasingdocument,
+        purchasingdocumentitem,
+        accountassignmentnumber,
+        purchasinghistorydocumenttype,
+        purchasinghistorydocumentyear,
+        purchasinghistorydocument,
+        purchasinghistorydocumentitem,
+        goodsmovementtype,
+        postingdate,
+        debitcreditcode,
+        quantity,
+        purordamountincompanycodecrcy,
+        purchaseorderamount
+    FROM silver_mm_zimmpurdochist
+
+),
+
+silver_zimmpurgdocitem_cte AS (
+    SELECT
+        purchasingdocument,
+        purchasingdocumentitem,
+        purchasingdocumentcategory,
+        material,
+        material_description,
+        quantity_numerator,
+        quantity_denominator,
+        plant,
+        materialgroup,
+        materialgroupname,
+        orderquantityunit,
+        baseunit,
+        free_item,
+        purchaserequisition,
+        purchaserequisitionitem,
+        purchasinginforecord,
+        orderquantity,
+        taxcode,
+        safetystockquantity,
+        netpriceamount
+    FROM silver_mm_zimmpurgdocitem
+),
+
+silver_zmmpurchasingdoc_cte AS (
+    SELECT
+        purchasingdocument,
+        purchasingdocumentcategory,
+        purchasingdocumenttype,
+        releasecode,
+        supplier,
+        suppliername,
+        purchasinggroup,
+        documentcurrency,
+        exchangerate,
+        purchasingdocumentorderdate,
+        paymentterms,
+        incotermsclassification,
+        purchasingdocumentcondition
+    FROM silver_mm_zmmpurchasingdoc
+)
+
+SELECT 
+
+    h.purchasingdocument, -- grain
+    h.purchasingdocumentitem, -- grain
+    h.accountassignmentnumber, -- grain
+    h.purchasinghistorydocumenttype, -- grain
+    h.purchasinghistorydocumentyear, -- grain
+    h.purchasinghistorydocument, -- grain
+    h.purchasinghistorydocumentitem, -- grain
+    h.goodsmovementtype,
+    h.postingdate,
+    h.debitcreditcode,
+    h.quantity,
+    h.purordamountincompanycodecrcy,
+    h.purchaseorderamount,
+    po.material,
+    po.material_description,
+    po.materialgroup,
+    po.materialgroupname,
+    po.baseunit,
+    po.orderquantityunit,
+    po.free_item,
+    po.purchaserequisition,
+    po.purchaserequisitionitem,
+    po.orderquantity,
+    po.netpriceamount,
+    po.taxcode,
+    po.safetystockquantity,
+    hd.supplier,
+    hd.suppliername,
+    hd.releasecode,
+    hd.purchasinggroup,
+    hd.documentcurrency,
+    hd.exchangerate,
+    hd.purchasingdocumentorderdate,
+    hd.paymentterms,
+    hd.incotermsclassification,
+    hd.purchasingdocumentcondition,
+    current_timestamp() as last_main_silver_modified_dt
+
+    FROM silver_zimmpurdochist_cte h
+    LEFT JOIN silver_zimmpurgdocitem_cte po
+    ON h.purchasingdocument = po.purchasingdocument
+    AND h.purchasingdocumentitem = po.purchasingdocumentitem
+    LEFT JOIN silver_zmmpurchasingdoc_cte hd
+    ON po.purchasingdocument = hd.purchasingdocument
